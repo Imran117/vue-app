159dffdec43a44be47468d82caa0df2809264458
Imran <umurzakov117ali@gmail.com>
2022-07-29 18:43:27 +0500

    finished commit

---
+++ src/_config.js
@@ -0,0 +1,32 @@
+export const links = [
+  {
+    title: 'Home',
+    class: 'header__nav-link',
+    url: '/',
+  },
+  {
+    title: 'Weather',
+    class: 'header__nav-link',
+    url: '/weather',
+  },
+];
+
+
+export const configIcon = [
+  {
+    img: require('./assets/images/icons/telegram.svg'),
+    url: 'https://t.me/Umurzakhov_Imran',
+  },
+  {
+    img: require(`./assets/images/icons/github.svg`),
+    url: 'https://github.com/Imran117',
+  },
+  {
+    img: require('./assets/images/icons/linkedin.svg'),
+    url: 'https://www.linkedin.com/in/imran-umurzakov-a5b655245/',
+  },
+  {
+    img: require('./assets/images/icons/facebook.svg'),
+    url: 'https://www.facebook.com/imron.umurzakov.1/',
+  },
+];
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+const { defineConfig } = require('@vue/cli-service')
+
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: 'https://Imran117.github.io/vue-app/'
+})
